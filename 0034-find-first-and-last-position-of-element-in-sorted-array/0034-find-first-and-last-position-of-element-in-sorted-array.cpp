6244035d382517550b6f71180186ce9322dbcb9c
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2023-10-09 21:33:39 +0900

    Time: 7 ms (51.15%), Space: 14 MB (14.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> answer = {-1, -1};
-        vector<int> flag = {-1, -1};
-        int size = nums.size();
-        //  O(log n) 이 아님. 수정 필요. 집가서 해결해보기 
-        for (int i = 0; i < size; i++)
-        {
-            if (nums[i] == target && flag[0] == -1)
-            {
-                answer[0] = i;
-                flag[0] = 1;
-            }
-            if (nums[size - i - 1] == target && flag[1] == -1)
-            {
-                answer[1] = size - i - 1;
-                flag[1] = 1;
-            }
-            if (flag[0] == 1 && flag[1] == 1)
-                break;
-        }
+
+        answer[0] = binarySearchTree(nums, target, 0);
+        answer[1] = binarySearchTree(nums, target, 1);
         return answer;
+    }
+
+    int binarySearchTree(vector<int>& nums, int target, bool isRight)
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid;
+        int findIndex = -1;
+
+        while (start <= end)
+        {
+            mid = start + ((end - start) / 2);
+            if (nums[mid] == target)
+            {
+                findIndex = mid;
+                if (isRight)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return findIndex;
     }
 };
